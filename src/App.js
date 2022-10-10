ba3c2650e1676fad63ec9b2d92b6804e6b723742
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-10 08:21:25 +0300

    Update - Add Base Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import styles from "./App.module.css";
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className={styles.App}>
+      <Container className={styles.Main}>
+        <Routes>
+          <Route path="/" element={""} />
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
