761c70a58add3bd1e138470d902d3430ffe044c8
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-11 21:26:44 +0300

    Update - Add Axios Interceptors for Token Refreshing & Non-Logged In Users

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosRes = axios.create();
+export const axiosReq = axios.create();
--- src/contexts/currentUserContext.js
+++ src/contexts/currentUserContext.js
@@ -1,5 +1,8 @@
 import axios from "axios";
+import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import { useNavigate } from "react-router";
+import { removeTokenTimestamp, shouldRefreshToken } from "../utils/utils";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
@@ -9,10 +12,11 @@ export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
 
 export const CurrentUserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
+  const navigate = useNavigate();
 
   const handleMount = async () => {
     try {
-      const { data } = await axios.get("auth/user/");
+      const { data } = await axiosRes.get("auth/user/");
       setCurrentUser(data);
     } catch (err) {
       console.log(err);
@@ -22,6 +26,52 @@ export const CurrentUserProvider = ({ children }) => {
   useEffect(() => {
     handleMount();
   }, []);
+
+  useMemo(() => {
+    axiosRes.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        if (err.response?.status === 401) {
+          try {
+            await axios.post("auth/token/refresh/");
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                navigate("/login/prompt");
+              }
+              return null;
+            });
+            removeTokenTimestamp();
+          }
+          return axios(err.config);
+        }
+        return Promise.reject(err);
+      }
+    );
+
+    axiosReq.interceptors.request.use(
+      async (config) => {
+        if (shouldRefreshToken()) {
+          try {
+            await axios.post("auth/token/refresh/");
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                navigate("/login/prompt");
+              }
+              return null;
+            });
+            removeTokenTimestamp();
+            return config;
+          }
+        }
+        return config;
+      },
+      (err) => {
+        return Promise.reject(err);
+      }
+    );
+  }, [navigate]);
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
