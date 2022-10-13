94d0d9852881f61da3ef714aaa21dc3bafc28aee
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-14 01:50:40 +0300

    Update - Add Signout Modal

---
+++ src/pages/auth/SignoutForm.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import React from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { useNavigate } from "react-router";
+import { useAuthModal, useSetAuthModal } from "../../contexts/authModalContext";
+import { useSetCurrentUser } from "../../contexts/currentUserContext";
+import { removeTokenTimestamp } from "../../utils/utils";
+
+const SignoutForm = () => {
+  const authModal = useAuthModal();
+  const setAuthModal = useSetAuthModal();
+  const setCurrentUser = useSetCurrentUser();
+  const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("auth/logout/");
+      setCurrentUser(null);
+      removeTokenTimestamp();
+      setAuthModal({ show: false });
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <Modal show={authModal.show} onHide={() => setAuthModal({ show: false })}>
+        <Modal.Header closeButton>
+          <Modal.Title>Sign Out</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form.Group>
+            <h3>Are you sure you wish to sign out?</h3>
+          </Form.Group>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button
+            onClick={() => setAuthModal({ show: false })}
+            className="w-75"
+            size="lg">
+            No, Take me back
+          </Button>
+          <Button
+            variant="danger"
+            onClick={handleSignOut}
+            className="w-75"
+            size="lg">
+            Sign Out
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default SignoutForm;
