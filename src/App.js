53c80d9dd8170e7bc81172877ea17a8d47968576
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-14 22:17:58 +0300

    Update - Add Basic Link Routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import styles from "./App.module.css";
-import { Container } from "react-bootstrap";
+import "./api/axiosDefaults";
 import { Route, Routes } from "react-router";
+import { Container } from "react-bootstrap";
+import AuthModal from "./pages/auth/AuthModal";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className={styles.App}>
-      <Container className={styles.Main}>
+    <>
+      <AuthModal />
+      <NavBar />
+      <Container fluid>
         <Routes>
-          <Route path="/" element={""} />
+          <Route index element={<h1>Home</h1>} />
+          <Route path="/feed" element={<h1>Feed</h1>} />
+          <Route path="/saved" element={<h1>Saved</h1>} />
+          <Route path="/profile/:id" element={<h1>Profile Page</h1>} />
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
       </Container>
-    </div>
+    </>
   );
 }
 
