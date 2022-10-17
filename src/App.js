937add527767f8e0e79e4a29289435fc64f23a5c
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-17 23:22:36 +0300

    Change - Change Bootstrap Layout

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import { Container } from "react-bootstrap";
 import AuthModal from "./pages/auth/AuthModal";
 import NavBar from "./components/NavBar";
 import ContactForm from "./pages/contact/ContactForm";
+import NewPostForm from "./pages/posts/NewPostForm";
 
 function App() {
   return (
     <>
       <AuthModal />
       <NavBar />
-      <Container fluid>
+      <Container className="my-3 text-center g-0">
         <Routes>
           <Route index element={<h1>Home</h1>} />
           <Route path="/feed" element={<h1>Feed</h1>} />
           <Route path="/saved" element={<h1>Saved</h1>} />
           <Route path="/profile/:id" element={<h1>Profile Page</h1>} />
+          <Route path="/new-post" element={<NewPostForm />} />
           <Route path="/contact-us" element={<ContactForm />} />
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
--- src/pages/contact/ContactForm.js
+++ src/pages/contact/ContactForm.js
@@ -51,199 +51,197 @@ const ContactForm = () => {
 
   return (
     <>
-      <Container className="my-3 text-center">
-        {formSent ? (
-          <>
-            <Card>
-              <Card.Header>
-                <h1>Submitted!</h1>
-              </Card.Header>
+      {formSent ? (
+        <>
+          <Card>
+            <Card.Header>
+              <h1>Submitted!</h1>
+            </Card.Header>
 
-              <Card.Body>
-                <p className="my-1">
-                  Your message has been sent, and will be reviewed by our team
-                  shortly.
-                </p>
-              </Card.Body>
+            <Card.Body>
+              <p className="my-1">
+                Your message has been sent, and will be reviewed by our team
+                shortly.
+              </p>
+            </Card.Body>
 
-              <Card.Footer className="btn-group p-0">
-                <Button variant="secondary" onClick={() => navigate(0)}>
-                  <i className="fa-solid fa-square-plus" /> Submit Another{" "}
-                  <i className="fa-solid fa-square-plus" />
-                </Button>
-                <NavLink end to="/" className="btn btn-primary">
-                  <i className="fa-solid fa-home" /> Home{" "}
-                  <i className="fa-solid fa-home" />
-                </NavLink>
-              </Card.Footer>
-            </Card>
-          </>
-        ) : (
-          <>
-            <Form className="card" onSubmit={handleSubmit}>
-              <Card.Header>
-                <h1>Contact Us</h1>
-                {currentUser?.is_admin && (
-                  <AdminButton
-                    leftIcon
-                    rightIcon
-                    href="admin/contactforms/contactform/"
-                    text="Submitted Contact Forms"
-                  />
-                )}
-              </Card.Header>
+            <Card.Footer className="btn-group p-0">
+              <Button variant="secondary" onClick={() => navigate(0)}>
+                <i className="fa-solid fa-square-plus" /> Submit Another{" "}
+                <i className="fa-solid fa-square-plus" />
+              </Button>
+              <NavLink end to="/" className="btn btn-primary">
+                <i className="fa-solid fa-home" /> Home{" "}
+                <i className="fa-solid fa-home" />
+              </NavLink>
+            </Card.Footer>
+          </Card>
+        </>
+      ) : (
+        <>
+          <Form className="card" onSubmit={handleSubmit}>
+            <Card.Header>
+              <h1>Contact Us</h1>
+              {currentUser?.is_admin && (
+                <AdminButton
+                  leftIcon
+                  rightIcon
+                  href="admin/contactforms/contactform/"
+                  text="Submitted Contact Forms"
+                />
+              )}
+            </Card.Header>
 
-              <Card.Body>
-                <Form.Group className="mb-3">
-                  <Form.Label className="d-none">Title</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Title"
-                    name="title"
-                    value={title}
-                    onChange={handleChange}
-                    className="text-center"
-                  />
-                  <FormError
-                    data={errors?.title}
-                    text="*Please provide a title for your message."
-                  />
-                  <hr />
-                </Form.Group>
+            <Card.Body>
+              <Form.Group className="mb-3">
+                <Form.Label className="d-none">Title</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Title"
+                  name="title"
+                  value={title}
+                  onChange={handleChange}
+                  className="text-center"
+                />
+                <FormError
+                  data={errors?.title}
+                  text="*Please provide a title for your message."
+                />
+                <hr />
+              </Form.Group>
 
-                <Row>
-                  <Col md="6">
-                    <Form.Group>
-                      <Form.Label className="d-none">First Name</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="First Name"
-                        name="first_name"
-                        value={first_name}
-                        onChange={handleChange}
-                        className="text-center"
-                      />
-                      <FormError
-                        data={errors?.first_name}
-                        text="(OPTIONAL) Please provide your first name."
-                      />
-                      <hr />
-                    </Form.Group>
-                  </Col>
+              <Row>
+                <Col md="6">
+                  <Form.Group>
+                    <Form.Label className="d-none">First Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="First Name"
+                      name="first_name"
+                      value={first_name}
+                      onChange={handleChange}
+                      className="text-center"
+                    />
+                    <FormError
+                      data={errors?.first_name}
+                      text="(OPTIONAL) Please provide your first name."
+                    />
+                    <hr />
+                  </Form.Group>
+                </Col>
 
-                  <Col md="6">
-                    <Form.Group>
-                      <Form.Label className="d-none">Last Name</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="Last Name"
-                        name="last_name"
-                        value={last_name}
-                        onChange={handleChange}
-                        className="text-center"
-                      />
-                      <FormError
-                        data={errors?.last_name}
-                        text="(OPTIONAL) Please provide your last name."
-                      />
-                      <hr />
-                    </Form.Group>
-                  </Col>
-                </Row>
+                <Col md="6">
+                  <Form.Group>
+                    <Form.Label className="d-none">Last Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Last Name"
+                      name="last_name"
+                      value={last_name}
+                      onChange={handleChange}
+                      className="text-center"
+                    />
+                    <FormError
+                      data={errors?.last_name}
+                      text="(OPTIONAL) Please provide your last name."
+                    />
+                    <hr />
+                  </Form.Group>
+                </Col>
+              </Row>
 
-                <Row>
-                  <Col md="6">
-                    <Form.Group>
-                      <Form.Label className="d-none">Username</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="Username"
-                        name="username"
-                        value={username}
-                        onChange={handleChange}
-                        className="text-center"
-                      />
-                      <FormError
-                        data={errors?.username}
-                        text="(OPTIONAL) Please enter your username."
-                      />
-                      <hr />
-                    </Form.Group>
-                  </Col>
+              <Row>
+                <Col md="6">
+                  <Form.Group>
+                    <Form.Label className="d-none">Username</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Username"
+                      name="username"
+                      value={username}
+                      onChange={handleChange}
+                      className="text-center"
+                    />
+                    <FormError
+                      data={errors?.username}
+                      text="(OPTIONAL) Please enter your username."
+                    />
+                    <hr />
+                  </Form.Group>
+                </Col>
 
-                  <Col md="6">
-                    <Form.Group>
-                      <Form.Label className="d-none">Email Address</Form.Label>
-                      <Form.Control
-                        type="email"
-                        placeholder="Email Address"
-                        name="email"
-                        value={email}
-                        onChange={handleChange}
-                        className="text-center"
-                      />
-                      <FormError
-                        data={errors?.email}
-                        text="*Please enter your email address."
-                      />
-                      <hr />
-                    </Form.Group>
-                  </Col>
-                </Row>
+                <Col md="6">
+                  <Form.Group>
+                    <Form.Label className="d-none">Email Address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Email Address"
+                      name="email"
+                      value={email}
+                      onChange={handleChange}
+                      className="text-center"
+                    />
+                    <FormError
+                      data={errors?.email}
+                      text="*Please enter your email address."
+                    />
+                    <hr />
+                  </Form.Group>
+                </Col>
+              </Row>
 
-                <Form.Group>
-                  <Form.Select
-                    className="text-center"
-                    name="reason"
-                    value={reason}
-                    onChange={handleChange}>
-                    <option value={0}>Support</option>
-                    <option value={1}>Bug Report</option>
-                    <option value={2}>Feedback</option>
-                    <option value={3}>Compliment</option>
-                    <option value={4}>Complaint</option>
-                  </Form.Select>
-                  <FormError
-                    data={errors?.reason}
-                    text="*Please select your reason for contacting us."
-                  />
-                  <hr />
-                </Form.Group>
+              <Form.Group>
+                <Form.Select
+                  className="text-center"
+                  name="reason"
+                  value={reason}
+                  onChange={handleChange}>
+                  <option value={0}>Support</option>
+                  <option value={1}>Bug Report</option>
+                  <option value={2}>Feedback</option>
+                  <option value={3}>Compliment</option>
+                  <option value={4}>Complaint</option>
+                </Form.Select>
+                <FormError
+                  data={errors?.reason}
+                  text="*Please select your reason for contacting us."
+                />
+                <hr />
+              </Form.Group>
 
-                <Form.Group>
-                  <Form.Label className="d-none">Contact Message</Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    rows={5}
-                    type="text"
-                    placeholder="Your Message"
-                    name="content"
-                    value={content}
-                    onChange={handleChange}
-                    className="text-center"
-                  />
-                  <FormError
-                    data={errors.content}
-                    text="*Please enter your message for our team."
-                  />
-                </Form.Group>
-                <FormError data={errors?.non_field_errors} />
-              </Card.Body>
+              <Form.Group>
+                <Form.Label className="d-none">Contact Message</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={5}
+                  type="text"
+                  placeholder="Your Message"
+                  name="content"
+                  value={content}
+                  onChange={handleChange}
+                  className="text-center"
+                />
+                <FormError
+                  data={errors.content}
+                  text="*Please enter your message for our team."
+                />
+              </Form.Group>
+              <FormError data={errors?.non_field_errors} />
+            </Card.Body>
 
-              <Card.Footer className="btn-group p-0">
-                <Button onClick={() => navigate(0)} variant="danger">
-                  <i className="fa-solid fa-trash" /> Clear{" "}
-                  <i className="fa-solid fa-trash" />
-                </Button>
-                <Button type="submit">
-                  <i className="fa-solid fa-paper-plane" /> Submit{" "}
-                  <i className="fa-solid fa-paper-plane" />
-                </Button>
-              </Card.Footer>
-            </Form>
-          </>
-        )}
-      </Container>
+            <Card.Footer className="btn-group p-0">
+              <Button onClick={() => navigate(0)} variant="danger">
+                <i className="fa-solid fa-trash" /> Clear{" "}
+                <i className="fa-solid fa-trash" />
+              </Button>
+              <Button type="submit">
+                <i className="fa-solid fa-paper-plane" /> Submit{" "}
+                <i className="fa-solid fa-paper-plane" />
+              </Button>
+            </Card.Footer>
+          </Form>
+        </>
+      )}
     </>
   );
 };
