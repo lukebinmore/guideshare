ee87ff02f5a63d29352a1de62ee4d91b6b465014
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-19 16:47:15 +0300

    Change - Refactor Login Form Form Inputs with FormInput Component

--- src/pages/auth/LoginForm.js
+++ src/pages/auth/LoginForm.js
@@ -4,9 +4,10 @@ import { Button, Form, Modal } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import IconText from "../../components/IconText";
 import FormError from "../../components/FormError";
+import FormInput from "../../components/FormInput";
 import { useAuthModal, useSetAuthModal } from "../../contexts/authModalContext";
 import { useSetCurrentUser } from "../../contexts/currentUserContext";
-import { setTokenTimestamp } from "../../utils/utils";
+import { collectFormData, setTokenTimestamp } from "../../utils/utils";
 
 const LoginForm = () => {
   const authModal = useAuthModal();
@@ -15,21 +16,12 @@ const LoginForm = () => {
   const navigate = useNavigate();
 
   const [errors, setErrors] = useState({});
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
-  const { username, password } = formData;
-
-  const handleChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    const formData = collectFormData(event);
+
     try {
       const { data } = await axios.post("auth/login/", formData);
       setCurrentUser(data.user);
@@ -50,33 +42,22 @@ const LoginForm = () => {
           </Modal.Header>
 
           <Modal.Body>
-            <Form.Group controlId="username">
-              <Form.Label className="d-none">Username</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Username"
-                name="username"
-                value={username}
-                onChange={handleChange}
-                className="text-center"
-              />
-              <FormError data={errors?.username} text="*Enter your username." />
-            </Form.Group>
+            <FormInput
+              placeholder="Username"
+              name="username"
+              errorData={errors?.username}
+              text="*Enter your username."
+              hr
+            />
 
-            <hr />
+            <FormInput
+              placeholder="Password"
+              type="password"
+              name="password"
+              errorData={errors?.password}
+              text="*Enter your password."
+            />
 
-            <Form.Group controlId="password">
-              <Form.Label className="d-none">Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={password}
-                onChange={handleChange}
-                className="text-center"
-              />
-              <FormError data={errors?.password} text="Enter your password." />
-            </Form.Group>
             <FormError data={errors?.non_field_errors} />
           </Modal.Body>
 
