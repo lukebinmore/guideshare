4de030c54dd7051cc521b6c7e40135706d21e020
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-19 16:47:58 +0300

    Change - Refactor Contact Form Inputs with FormInput Component

--- src/pages/contact/ContactForm.js
+++ src/pages/contact/ContactForm.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Button, Card, Col, Form, Row } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { NavLink } from "react-router-dom";
@@ -7,6 +7,8 @@ import FormError from "../../components/FormError";
 import AdminButton from "../../components/AdminButton";
 import { useCurrentUser } from "../../contexts/currentUserContext";
 import IconText from "../../components/IconText";
+import FormInput from "../../components/FormInput";
+import { collectFormData } from "../../utils/utils";
 
 const ContactForm = () => {
   const currentUser = useCurrentUser();
@@ -14,33 +16,11 @@ const ContactForm = () => {
 
   const [formSent, setFormSent] = useState(false);
   const [errors, setErrors] = useState({});
-  const [formData, setFormData] = useState({
-    title: "",
-    username: "",
-    email: "",
-    first_name: "",
-    last_name: "",
-    reason: 0,
-    content: "",
-  });
-  const { title, email, username, first_name, last_name, reason, content } =
-    formData;
-
-  useEffect(() => {
-    setFormData((prevFormData) => {
-      return { ...prevFormData, username: currentUser?.username };
-    });
-  }, [navigate, currentUser]);
-
-  const handleChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    const formData = collectFormData(event);
 
     try {
       await axios.post("contact-form/", formData);
@@ -68,12 +48,7 @@ const ContactForm = () => {
 
             <Card.Footer className="btn-group p-0">
               <Button variant="secondary" onClick={() => navigate(0)}>
-                <IconText
-                  text="Submit Another"
-                  icon="square-plus"
-                  left
-                  right
-                />
+                <IconText text="Submit Another" icon="square-plus" left right />
               </Button>
               <NavLink end to="/" className="btn btn-primary">
                 <IconText text="Home" icon="home" left right />
@@ -97,140 +72,83 @@ const ContactForm = () => {
             </Card.Header>
 
             <Card.Body>
-              <Form.Group className="mb-3">
-                <Form.Label className="d-none">Title</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Title"
-                  name="title"
-                  value={title}
-                  onChange={handleChange}
-                  className="text-center"
-                />
-                <FormError
-                  data={errors?.title}
-                  text="*Please provide a title for your message."
-                />
-                <hr />
-              </Form.Group>
+              <FormInput
+                placeholder="Title"
+                name="title"
+                errorData={errors?.title}
+                text="*Please enter a title for your message"
+                hr
+              />
 
               <Row>
-                <Col md="6">
-                  <Form.Group>
-                    <Form.Label className="d-none">First Name</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="First Name"
-                      name="first_name"
-                      value={first_name}
-                      onChange={handleChange}
-                      className="text-center"
-                    />
-                    <FormError
-                      data={errors?.first_name}
-                      text="(OPTIONAL) Please provide your first name."
-                    />
-                    <hr />
-                  </Form.Group>
+                <Col md>
+                  <FormInput
+                    placeholder="First Name"
+                    name="first_name"
+                    errorData={errors?.first_name}
+                    text="(OPTIONAL) Please enter your first name."
+                    hr
+                  />
                 </Col>
 
-                <Col md="6">
-                  <Form.Group>
-                    <Form.Label className="d-none">Last Name</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="Last Name"
-                      name="last_name"
-                      value={last_name}
-                      onChange={handleChange}
-                      className="text-center"
-                    />
-                    <FormError
-                      data={errors?.last_name}
-                      text="(OPTIONAL) Please provide your last name."
-                    />
-                    <hr />
-                  </Form.Group>
+                <Col md>
+                  <FormInput
+                    placeholder="Last Name"
+                    name="last_name"
+                    errorData={errors?.last_name}
+                    text="(OPTIONAL) Please enter your last name."
+                    hr
+                  />
                 </Col>
               </Row>
 
               <Row>
-                <Col md="6">
-                  <Form.Group>
-                    <Form.Label className="d-none">Username</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="Username"
-                      name="username"
-                      value={username}
-                      onChange={handleChange}
-                      className="text-center"
-                    />
-                    <FormError
-                      data={errors?.username}
-                      text="(OPTIONAL) Please enter your username."
-                    />
-                    <hr />
-                  </Form.Group>
+                <Col md>
+                  <FormInput
+                    placeholder="Username"
+                    name="username"
+                    errorData={errors?.username}
+                    text="(OPTIONAL) Please enter your username."
+                    initialValue={currentUser?.username}
+                    hr
+                  />
                 </Col>
 
-                <Col md="6">
-                  <Form.Group>
-                    <Form.Label className="d-none">Email Address</Form.Label>
-                    <Form.Control
-                      type="email"
-                      placeholder="Email Address"
-                      name="email"
-                      value={email}
-                      onChange={handleChange}
-                      className="text-center"
-                    />
-                    <FormError
-                      data={errors?.email}
-                      text="*Please enter your email address."
-                    />
-                    <hr />
-                  </Form.Group>
+                <Col md>
+                  <FormInput
+                    placeholder="Email"
+                    name="email"
+                    type="email"
+                    errorData={errors?.email}
+                    text="*Please enter your email address."
+                    hr
+                  />
                 </Col>
               </Row>
 
-              <Form.Group>
-                <Form.Label className="d-none">Reason For Contact</Form.Label>
-                <Form.Select
-                  className="text-center"
-                  name="reason"
-                  value={reason}
-                  onChange={handleChange}>
-                  <option value={0}>Support</option>
-                  <option value={1}>Bug Report</option>
-                  <option value={2}>Feedback</option>
-                  <option value={3}>Compliment</option>
-                  <option value={4}>Complaint</option>
-                </Form.Select>
-                <FormError
-                  data={errors?.reason}
-                  text="*Please select your reason for contacting us."
-                />
-                <hr />
-              </Form.Group>
+              <FormInput
+                placeholder="Reason For Contact"
+                type="select"
+                name="reason"
+                errorData={errors?.reason}
+                text="*Please select your reason for contacting us."
+                hr>
+                <option value={0}>Support</option>
+                <option value={1}>Bug Report</option>
+                <option value={2}>Feedback</option>
+                <option value={3}>Compliment</option>
+                <option value={4}>Complaint</option>
+              </FormInput>
 
-              <Form.Group>
-                <Form.Label className="d-none">Contact Message</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={5}
-                  type="text"
-                  placeholder="Your Message"
-                  name="content"
-                  value={content}
-                  onChange={handleChange}
-                  className="text-center"
-                />
-                <FormError
-                  data={errors?.content}
-                  text="*Please enter your message for our team."
-                />
-              </Form.Group>
+              <FormInput
+                placeholder="Your Message"
+                as="textarea"
+                rows={5}
+                name="content"
+                errorData={errors?.content}
+                text="*Please enter your message for our team."
+                hr
+              />
               <FormError data={errors?.non_field_errors} />
             </Card.Body>
 
