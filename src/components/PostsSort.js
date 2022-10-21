a91a431d0f1183825d665a9cee31e566dc4d85ce
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-21 08:31:29 +0300

    Update - Refactor Posts Sort into Separate Component

---
+++ src/components/PostsSort.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { Col, Dropdown, Form, Row } from "react-bootstrap";
+import {
+  useSearchFilterSort,
+  useSetSearchFilterSort,
+} from "../contexts/searchFilterSortContext";
+import styles from "../styles/PostsSort.module.css";
+
+const sortValues = [
+  { title: "Title", eventKey: "title" },
+  { title: "Date Created", eventKey: "created_at" },
+  { title: "Likes", eventKey: "likes_count" },
+  { title: "Dislikes", eventKey: "dislikes_count" },
+];
+
+const PostsSort = () => {
+  const { sort } = useSearchFilterSort();
+  const setSearchFilterSort = useSetSearchFilterSort();
+
+  const handleChange = (event) => {
+    const newOrder = () => {
+      if (sort === event) {
+        return "-" + event;
+      } else if (sort === "-" + event) {
+        return event;
+      } else {
+        return event;
+      }
+    };
+
+    setSearchFilterSort((prevData) => ({ ...prevData, sort: newOrder() }));
+  };
+
+  return (
+    <Dropdown drop="down" align="end" onSelect={handleChange}>
+      <Form.Label className="d-none">Post Sorting</Form.Label>
+      <Dropdown.Toggle className="w-25">
+        <p className="d-md-inline d-none">Sort </p>
+        <i className="fa-solid fa-sort" />
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu align="end">
+        {sortValues.map((option, idx) => (
+          <Dropdown.Item key={idx} as="div" eventKey={option.eventKey}>
+            <Row>
+              <Col xs="auto">
+                {sort.includes(option.eventKey) && (
+                  <i
+                    className={`${styles.DirectionIcon} p-1 fa-solid fa-${
+                      sort.includes("-") ? "angle-down" : "angle-up"
+                    }`}
+                  />
+                )}
+              </Col>
+              <Col className="text-end">
+                <p className="m-0 p-1">{option.title}</p>
+              </Col>
+            </Row>
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
+
+export default PostsSort;
