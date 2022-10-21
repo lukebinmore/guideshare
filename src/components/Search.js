cd527a7ad05393113a7edbace489774e20ffcce3
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-21 09:17:21 +0300

    Update - Refactor SearchQueryFilter Component into Search Component

---
+++ src/components/Search.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import {
+  useSearchFilterSort,
+  useSetSearchFilterSort,
+} from "../contexts/searchFilterSortContext";
+
+const Search = (props) => {
+  const { query } = useSearchFilterSort();
+  const setSearchFilterSort = useSetSearchFilterSort();
+
+  const [searchQuery, setSearchQuery] = useState(query);
+
+  useEffect(() => {
+    setSearchQuery(query);
+  }, [query]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setSearchFilterSort((prevValues) => ({
+      ...prevValues,
+      query: searchQuery,
+    }));
+  };
+
+  return (
+    <Form className="d-flex h-100 align-items-center" onSubmit={handleSubmit}>
+      <InputGroup>
+        <Form.Label className="d-none">Search</Form.Label>
+        <Form.Control
+          type="search"
+          placeholder="Search"
+          name="query"
+          value={searchQuery}
+          onChange={(event) => setSearchQuery(event.target.value)}
+          className="text-center form-control"
+        />
+        <Button type="submit">
+          <i className="fa-solid fa-search" />
+        </Button>
+      </InputGroup>
+    </Form>
+  );
+};
+
+export default Search;
