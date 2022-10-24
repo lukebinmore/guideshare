2bd8d6037ccbeb8f59d93e03170167b0f317c0c0
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-24 15:50:38 +0300

    Update - Add Comment Delete Option Config

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
+import { axiosReq, axiosRes } from "../../api/axiosDefaults";
 import {
   EditDeleteDropdown,
   ProfileButton,
@@ -8,6 +9,7 @@ import {
 import { useBreakpoints } from "../../hooks";
 
 const Comment = (props) => {
+  const [commentData, setCommentData] = useState(props);
   const {
     id,
     owner,
@@ -20,34 +22,49 @@ const Comment = (props) => {
     likes_count,
     dislike_id,
     dislikes_count,
-  } = props;
+  } = commentData;
   const { md } = useBreakpoints();
 
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`);
+      setCommentData({});
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <Card>
-      <Card.Header className="d-flex justify-content-between align-items-center p-2">
-        <ProfileButton
-          profile_id={profile_id}
-          src={md && profile_picture}
-          username={owner}
-          small
-        />
-        <p className="m-0 text-muted">{created_at}</p>
-      </Card.Header>
+    <>
+      {commentData?.id && (
+        <Card className="mb-2">
+          <Card.Header className="d-flex justify-content-between align-items-center p-2">
+            <ProfileButton
+              profile_id={profile_id}
+              src={md && profile_picture}
+              username={owner}
+              small
+            />
+            <p className="m-0 text-muted">{created_at}</p>
+          </Card.Header>
 
-      <Card.Body className="d-flex justify-content-between align-items-center py-2 px-3">
-        <div className="text-start">{content}</div>
-        {is_owner && <EditDeleteDropdown solid />}
-      </Card.Body>
+          <Card.Body className="d-flex justify-content-between align-items-center py-2 px-3">
+            <div className="text-start">{content}</div>
+            {is_owner && (
+              <EditDeleteDropdown solid handleDelete={handleDelete} />
+            )}
+          </Card.Body>
 
-      <VoteFooter
-        comment_id={id}
-        like_id={like_id}
-        likes_count={likes_count}
-        dislike_id={dislike_id}
-        dislikes_count={dislikes_count}
-      />
-    </Card>
+          <VoteFooter
+            comment_id={id}
+            like_id={like_id}
+            likes_count={likes_count}
+            dislike_id={dislike_id}
+            dislikes_count={dislikes_count}
+          />
+        </Card>
+      )}
+    </>
   );
 };
 
