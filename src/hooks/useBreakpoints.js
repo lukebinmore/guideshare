dbf17c4d35aeec7287a48f41043dfa3f6471deb5
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-28 16:13:49 +0300

    Update - Add Hooks Code Commenting

--- src/hooks/useBreakpoints.js
+++ src/hooks/useBreakpoints.js
@@ -1,12 +1,15 @@
 import { useMediaQuery } from "react-responsive";
 
 const useBreakpoints = () => {
+  /* Uses react-responsive to create breakpoints at different sizes. Sizes
+  have been set to match Bootstrap breakpoints. */
   const sm = useMediaQuery({ query: "(min-width: 576px)" });
   const md = useMediaQuery({ query: "(min-width: 768px)" });
   const lg = useMediaQuery({ query: "(min-width: 992px)" });
   const xl = useMediaQuery({ query: "(min-width: 1200px)" });
   const xxl = useMediaQuery({ query: "(min-width: 1400px)" });
 
+  /* Returns breakpoints */
   return { sm, md, lg, xl, xxl };
 };
 
--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -4,9 +4,13 @@ import { useNavigate } from "react-router";
 import { useSetAuthModal } from "../contexts/AuthModalContext";
 
 const useRedirect = (target) => {
+  /* Using the react-router-dom hook useNavigate() and the custom hook
+  useSetAuthModal() */
   const navigate = useNavigate();
   const setAuthModal = useSetAuthModal();
 
+  /* Function that attempts a token refresh on page change, to see if the user
+  is logged in. Redirects to appripriate page if not. */
   useEffect(() => {
     const handleMount = async () => {
       try {
