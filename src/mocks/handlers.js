e5f12e0dfd4fa150b097e521566c0bebd00dd3d8
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-10-28 11:19:44 +0300

    Update - Prepare for Jest Unit Testing

---
+++ src/mocks/handlers.js
@@ -0,0 +1,43 @@
+import { rest } from "msw";
+
+const baseURL = process.env.REACT_APP_API_URL;
+
+export const handlers = [
+  rest.get(`${baseURL}auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 1,
+        username: "admin",
+        email: "admin@guideshare.com",
+        first_name: "Admin",
+        last_name: "Account",
+        profile_id: 1,
+        profile_picture:
+          "https://res.cloudinary.com/binmore-cloudinary/image/upload/v1/guideshare/profiles/2/0e10192f5be11713ffaa7c298660f809_tdhmzg",
+        is_admin: true,
+      })
+    );
+  }),
+  rest.post(`${baseURL}auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+  rest.get(`${baseURL}saved-following/*`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        following: [3],
+        saved_posts: [1],
+      })
+    );
+  }),
+  rest.put(`${baseURL}saved-following/*`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+  rest.get(`${baseURL}posts/categories/`, (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { id: 1, title: "Category 1" },
+        { id: 2, title: "Category 2" },
+      ])
+    );
+  }),
+];
